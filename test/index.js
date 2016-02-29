c0ba1f952042a2e10e92118e8985d26d87270d38
Jim de Beer <jim@vigour.io>
2016-02-29 14:00:46 +0100

    added tests, refactor some code / names

--- test/index.js
+++ test/index.js
@@ -3,13 +3,24 @@ var test = require('tape')
 var http = require('http')
 var Api = require('..')
 
-var mockServer = http.createServer(function(res, req) {
+stringResponse.on('response', (data) => {
+  console.log('ENCODE', 'on.response', 'data', data)
+})
 
-  console.log('lulzzzz')
-
-}).listen(3333)
-
-test('can make an http request', function (t) {
+test('can make an http formData request', function (t) {
   t.plan(1)
+  var stringResponse = new Api({
+    url: 'http://demo4427401.mockable.io/stringResponse',
+    method: 'get', // typo
+    encodeJson: false,
+    headers: {
+      'Accept': 'text/plain'
+    }
+  })
+
+  formLogin.request({
+    username: 'valerio12345@mailinator.com',
+    password: 'password'
+  })
 
 })
