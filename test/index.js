d2ad3ebeeea8976be552eb57b88b1ea79574068b
Jim de Beer <jim@vigour.io>
2016-03-01 15:53:09 +0100

    renamed duplex to stream , removed current request , refactor abort, cleaning up poll

--- test/index.js
+++ test/index.js
@@ -24,3 +24,103 @@ test('can make an http formData request', function (t) {
   })
 
 })
+
+/*
+"api": {
+      "scraper": {
+        "url": "http://adm-scraper.vigour.io/",
+        "encodeJson": true,
+        "headers": {
+          "accept-encoding": "*",
+          "accept": "application/json"
+        }
+      },
+      "emailConfirm": {
+        "url": "http://adm-proxy.vigour.io/email/confirm/",
+        "encodeJson": true,
+        "headers": {
+          "accept": "application/json"
+        }
+      },
+      "newsletter": {
+        "url": "http://adm-proxy.vigour.io/user/",
+        "encodeJson": true,
+        "httpMethod": "put",
+        "headers": {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        }
+      },
+      "video": {
+        "getSource": {
+          "url": "http://adm-proxy.vigour.io/video/",
+          "encodeJson": true,
+          "headers": {
+            "Accept": "application/json"
+          }
+        },
+        "sendPlayback": {
+          "url": "http://adm-proxy.vigour.io/video/",
+          "httpMethod": "post",
+          "encodeJson": true,
+          "headers": {
+            "Accept": "application/json"
+          }
+        },
+        "sendEvent": {
+          "url": "http://adm-proxy.vigour.io/video/event/",
+          "httpMethod": "post",
+          "encodeJson": true,
+          "headers": {
+            "Accept": "application/json"
+          }
+        }
+      },
+      "password": {
+        "forgot": {
+          "url": "http://adm-proxy.vigour.io/password/forgot/",
+          "encodeJson": true,
+          "headers": {
+            "Accept": "application/json"
+          }
+        },
+        "reset": {
+          "url": "http://adm-proxy.vigour.io/password/reset",
+          "encodeJson": true,
+          "httpMethod": "post",
+          "headers": {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          }
+        }
+      },
+      "auth": {
+        "verify": {
+          "url": "http://adm-proxy.vigour.io/verify/",
+          "encodeJson": true,
+          "headers": {
+            "Accept": "application/json"
+          }
+        },
+        "login": {
+          "url": "http://adm-proxy.vigour.io/login/api",
+          "httpMethod": "post",
+          "encodeJson": true,
+          "headers": {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          }
+        },
+        "register": {
+          "url": "http://adm-proxy.vigour.io/register",
+          "httpMethod": "post",
+          "encodeJson": true,
+          "headers": {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          }
+        }
+      }
+    }
+  },
+*/
