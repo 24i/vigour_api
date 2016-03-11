430cc384a42cf4dd2ecc9311705d53c3cf6e4a44
Jim de Beer <jim@vigour.io>
2016-03-11 13:02:07 +0100

    correct api test

--- test/server.js
+++ test/server.js
@@ -2,7 +2,14 @@
 var http = require('http')
 var server = module.exports = http.createServer((req, res) => {
   var body = ''
-  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Content-Type', 'text/plain')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  })
   req.on('data', (chunk) => { body += chunk })
   req.on('end', () => {
     var parsed
